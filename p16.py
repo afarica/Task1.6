2203fbac13ec797d08577388243c4f189fc2f8f0
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 02:12:25 +0600

    Update p16.py

--- p16.py
+++ p16.py
@@ -1,3 +1,4 @@
+# Write a function bigger_guy that takes in wo numbers and returns the bigger one.
 first_number=int(input())
 second_number=int(input())
 defining_variable=first_number-second_number
